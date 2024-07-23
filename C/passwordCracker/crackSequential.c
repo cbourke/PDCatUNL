d9d72800aef7f4138c984a4b000d0d664395fcfb
Chris Bourke <cbourke@cse.unl.edu>
2024-07-23 16:28:05 -0500

    reduced dissimilarities in sequential vs openmp

--- C/passwordCracker/crackSequential.c
+++ C/passwordCracker/crackSequential.c
@@ -12,24 +12,25 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  initDictionary();
-
   char *hashedPassword = argv[1];
   char hash[67];
   char password[255];
-  int counter = 0;
+  int i, j;
 
+  printf("Loading dictionary with %d words...\n", NUM_WORDS);
+  initDictionary();
+
+  //search all 2 digit appended numbers...
   int m = 100;
-  for(int i=0; i<NUM_WORDS; i++) {
-    for(int j=0; j<m; j++) {
+
+  for(i=0; i<NUM_WORDS; i++) {
+    for(j=0; j<m; j++) {
       //create password padded with 2 digits
       sprintf(password, "%s%02d", DICTIONARY[i], j);
       sha256toHex(hash, password);
-      counter++;
 
       if(strcmp(hash, hashedPassword) == 0) {
         printf("Cracked password: %s => %s\n", hashedPassword, password);
-        printf("Hashes Computed: %d\n", counter);
         exit(0);
       }
 
@@ -37,7 +38,6 @@ int main(int argc, char **argv) {
   }
 
   printf("Password not found!\n");
-  printf("Hashes Computed: %d\n", counter);
 
   return 0;
 
